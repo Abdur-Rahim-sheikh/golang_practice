0aa137f806197a6d14a1cfeb58974ef735683c6d
Abdur-Rahim-Sheikh <abi.rahim.sheikh@gmail.com>
2026-08-26 22:43:36 +0600

    updated .env file read

---
+++ backend/config/config.go
@@ -0,0 +1,52 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	Version     string
+	ServiceName string
+	HttpPort    int
+}
+
+var conf Config
+
+func LoadConfig() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("failed to load the .env file", err)
+		os.Exit(1)
+	}
+	version := os.Getenv("VERSION")
+	if version == "" {
+		fmt.Println("Version is required")
+		os.Exit(1)
+	}
+	service_name := os.Getenv("SERVICE_NAME")
+	if service_name == "" {
+		fmt.Println("service_name is required")
+		os.Exit(1)
+	}
+	http_port := os.Getenv("HTTP_PORT")
+	port, err := strconv.ParseInt(http_port, 10, 32)
+	if http_port == "" || err != nil {
+		fmt.Println("valid HTTP_PORT is required", err)
+		os.Exit(1)
+	}
+
+	conf = Config{
+		Version:     version,
+		ServiceName: service_name,
+		HttpPort:    int(port),
+	}
+}
+
+func GetConfig() Config {
+	LoadConfig()
+	return conf
+}
